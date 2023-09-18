e70cf10005f706794e5b0ed689d93278c0554d5e
dank_meme01 <kirill.babikov28@gmail.com>
2023-09-18 15:59:50 +0200

    fix cube having wrong rotation when upside down

--- src/ppa/ppa_base.cpp
+++ src/ppa/ppa_base.cpp
@@ -13,7 +13,11 @@ void PPAEngine::updatePlayer(
         player.first->setVisible(true);
         auto scale = data.player1.isMini ? 0.6f : 1.0f;
         player.first->setScale(scale);
-        player.first->setScaleY(scale * (data.player1.isUpsideDown ? -1 : 1));
+
+        if (data.player1.gameMode != IconGameMode::CUBE) { // cube reacts differently to m_isUpsideDown i think
+            player.first->setScaleY(scale * (data.player1.isUpsideDown ? -1 : 1));
+        }
+        
         // player.first->setScaleY(abs(player.first->getScaleY()) * (data.player1.isUpsideDown ? -1 : 1));
         player.first->tick(data.player1.gameMode);
         updateSpecificPlayer(player.first, data.player1, frameDelta, playerId, false);
@@ -26,7 +30,11 @@ void PPAEngine::updatePlayer(
         player.second->setVisible(true);
         auto scale = data.player2.isMini ? 0.6f : 1.0f;
         player.second->setScale(scale);
-        player.second->setScaleY(scale * (data.player2.isUpsideDown ? -1 : 1));
+
+        if (data.player1.gameMode != IconGameMode::CUBE) {
+            player.second->setScaleY(scale * (data.player2.isUpsideDown ? -1 : 1));
+        }
+
         // player.second->setScaleY(abs(player.second->getScaleY()) * (data.player2.isUpsideDown ? -1 : 1));
         player.second->tick(data.player2.gameMode);
         updateSpecificPlayer(player.second, data.player2, frameDelta, playerId, true);
