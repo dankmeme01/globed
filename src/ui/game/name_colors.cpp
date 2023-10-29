77d5be240ed48b80474f401501b48b3516f217b3
dank_meme01 <kirill.babikov28@gmail.com>
2023-10-29 23:00:29 +0100

    add croozy "ruler of everything" croozington color

--- src/ui/game/name_colors.cpp
+++ src/ui/game/name_colors.cpp
@@ -1,5 +1,7 @@
 #include "name_colors.hpp"
 
+using namespace geode::prelude;
+
 cocos2d::ccColor3B pickNameColor(std::string name) {
     // convert to lowercase
     std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) {
@@ -7,11 +9,13 @@ cocos2d::ccColor3B pickNameColor(std::string name) {
     });
 
     if (name == "dankmeme01") {
-        return {.r = 204, .g = 52, .b = 235};
+        return ccc3(204, 52, 235);
     } else if (name == "robtop") {
-        return {.r = 235, .g = 222, .b = 52};
+        return ccc3(235, 222, 52);
     } else if (name == "ca7x3") {
-        return {.r = 255, .g = 102, .b = 204};
+        return ccc3(255, 102, 204);
+    } else if (name == "croozington") {
+        return ccc3(185, 0, 255);
     }
-    return {255, 255, 255};
+    return ccc3(255, 255, 255);
 }
